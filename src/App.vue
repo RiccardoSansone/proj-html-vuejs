73eb263afc1b25c574fb309ec0f485a2510badb1
RiccardoSansone <r.sansone@gmail.com>
2023-10-03 13:31:46 +0200

    add part of header

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,47 @@ import './assets/scss/style.scss'
 
 <!-- header -->
 <header>
+
+    <!-- greenLine -->
+    <div id="greenLine">
+        <!-- smallInfo -->
+        <div class="smallInfo d-flex justify-between">
+            <!-- left menu -->
+            <div class="leftSmall d-flex gap-1 align-center">
+
+                <a href="#">
+                    <i class="fa-solid fa-location-dot"></i>
+                    North baukhula, tala, USA
+                </a>
+
+                <a href="#">
+                    <i class="fa-solid fa-envelope"></i>
+                    demo@example.com
+                </a>
+
+            </div>
+            <!-- right menu -->
+            <div class="rightSmall d-flex gap-1 align-center">
+
+                <a href="#">
+                    <i class="fa-regular fa-clock"></i>
+                    8.30AM-8.30PM
+                </a>
+
+                <div class="d-flex gap-05">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-pinterest-p"></i></a>
+                </div>
+
+            </div>
+        </div>
+        <!-- /smallInfo -->
+    </div>
+    <!-- /greenLine -->
+    <!-- /////////////// -->
+
 </header>
 <!-- /header -->
 <!-- /////////////// -->
