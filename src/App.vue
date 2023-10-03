df101408a23b8d80d6eb5b824bf43c05ae46456e
RiccardoSansone <r.sansone@gmail.com>
2023-10-03 15:10:47 +0200

    draft header

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,44 @@ import './assets/scss/style.scss'
     <!-- /greenLine -->
     <!-- /////////////// -->
 
+    <!-- navbar -->
+    <nav class="nav">
+        <!-- container -->
+        <div class="navContainer d-flex align-center">
+            <!-- logo -->
+            <div class="logo flex-start">
+                <img src="./assets/img/logo.png" alt="">
+            </div>
+            <!-- central menu -->
+            <div class="navMenu d-flex gap-05">
+                <p>f</p>
+                <p>f</p>
+                <p>ff</p>
+                <p>ff</p>
+                <p>fff</p>
+            </div>
+            <!-- right menu -->
+            <div class="cart d-flex gap-05 align-center">
+                <!-- circle icon -->
+                <div class="search d-flex align-center justify-center">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <!-- cart -->
+                <div class="cartIcon">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <div class="zero d-flex justify-center align-center">0</div>
+                </div>
+                <!-- button -->
+                <button class="btn bg-secondary">ORDER NOW</button>
+
+
+            </div>
+        </div>
+
+    </nav>
+    <!-- /navbar -->
+    <!-- /////////////// -->
+
 </header>
 <!-- /header -->
 <!-- /////////////// -->
