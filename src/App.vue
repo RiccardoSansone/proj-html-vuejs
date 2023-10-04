2a03cd4a3e7094fc864a9de303ef385822424f06
RiccardoSansone <r.sansone@gmail.com>
2023-10-04 15:04:33 +0200

    add partial footer

--- src/App.vue
+++ src/App.vue
@@ -568,6 +568,99 @@ import './assets/scss/style.scss'
 
 <!-- footer -->
 <footer>
+    <!-- footer top -->
+    <section class="footerTop">
+        <!-- container -->
+        <div class="footContainer d-flex justify-between">
+
+            <!-- first col -->
+            <div class="col-logo column gap-1">
+                <img src="./assets/img/footer-logo.png" alt="">
+                <p class="line-height-01 color-white"> amet recusandae tempore atque? Blanditiis, sit?</p>
+                <!-- social -->
+                <div class="social d-flex gap-05">
+                  <a class="cube d-flex align-center justify-center" href="#"><i class="fa-solid fa-glass-cheers"></i></a>
+                </div>
+            </div>
+
+            <!-- second col -->
+            <div class="emergency column gap-1">
+                <h3 class="color-white">lorem</h3>
+                <a href="#">lorem</a>
+                <a href="#">lorem</a>
+                <a href="#">lorem</a>
+                <a href="#">lorem</a>
+                <a href="#">lorem</a>
+                <a href="#">lorem</a>
+            </div>
+
+            <!-- third col -->
+            <div class="blog column gap-1">
+                <h3 class="color-white">
+                    lorem
+                </h3>
+                <!-- row -->
+                <div class="d-flex gap-1">
+                    <div>
+                         <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <div class="column gap-02">
+                        <p>lorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem lorem</p>
+                        <small>
+                            date 12 34 11
+                        </small>
+                    </div>
+                </div>
+                <!-- row -->
+                <div class="d-flex gap-1">
+                    <div>
+                         <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <div class="column gap-02">
+                        <p>lorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem loremlorem lorem lorem lorem</p>
+                        <small>
+                            date 12 34 11
+                        </small>
+                    </div>
+                </div>
+
+            </div>
+
+            <!-- fourth col -->
+            <div class="gallery column gap-2">
+                <h3 class="color-white">lorem</h3>
+                <!-- container -->
+                <div class="wrap-gallery d-flex wrap gap-05">
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                    <!-- img -->
+                    <div class="img-gallery">
+                        <img src="./assets/img/blo1-70x70.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /container -->
+    </section>
+
 </footer>
 <!-- /footer -->
 <!-- /////////////// -->
